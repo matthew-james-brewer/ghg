77258e8e1c01ee503ee61b4e847156980e60942b
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2026-04-27 22:52:18 -0500

    shut up

--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,7 @@ harch=$(uname -m)
 fname="$harch-linux-gnu-ghg.tar.gz"
 curl "https://github.com/matthew-james-brewer/ghg/releases/latest/download/$fname" -Lo /tmp/$fname
 
-read -p "Where is it installed? " ipath < /dev/stdin
+read -p "Where is it installed? " ipath < /dev/tty
 
 for installed in $(tar tzf /tmp/$fname | sed "s/^compiled//"); do
  rm $ipath/$installed
